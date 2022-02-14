a7db516ec8e4b93c54b65c625f8c16a9269ab998
ponyjackal <pony1125@outlook.com>
2022-02-14 08:52:29 -0800

    remove TODO comments

--- src/client/js/utils/global.js
+++ src/client/js/utils/global.js
@@ -20,52 +20,45 @@ window.GlobalStateManager = {
   },
 
   initialize: async function () {  
-    // TODO need to refactor this
-    if (false) {
-      // TODO this.isCrossChainEnabled()) {
-      // TODO crossChain not supported in TradingView
-    } else {
-      const network = TokenListManager.getCurrentNetworkConfig();
+    const network = TokenListManager.getCurrentNetworkConfig();
 
-      // get swapConfig from localstorage
-      const swapConfig = store.get('swap');
-      const defaultSwapConfig = {
-        from: TokenListManager.findTokenById(network.defaultPair.from),
-        to: TokenListManager.findTokenById(network.defaultPair.to),
-        fromChain: network.name,
-        toChain: network.name,
-      };
-  
-      const swap = swapConfig ? swapConfig : defaultSwapConfig;
-      
-      this.updateSwapConfig(swap);
+    // get swapConfig from localstorage
+    const swapConfig = store.get('swap');
+    const defaultSwapConfig = {
+      from: TokenListManager.findTokenById(network.defaultPair.from),
+      to: TokenListManager.findTokenById(network.defaultPair.to),
+      fromChain: network.name,
+      toChain: network.name,
+    };
 
-      // init bridgeConfig
-      const bridgeConfig = store.get('bridge');
-      const crossChainNetworks = _.filter(
-        window.NETWORK_CONFIGS,
-        (v) => v.enabled && v.crossChainSupported,
-      );
-      const toChain = crossChainNetworks.find(
-        (v) => v.chainId !== network.chainId,
-      );
+    const swap = swapConfig ? swapConfig : defaultSwapConfig;
+    
+    this.updateSwapConfig(swap);
 
-      const defaultBridgeConfig = {
-        from: TokenListManager.findTokenById(network.supportedCrossChainTokens[0]),
-        to: TokenListManager.findTokenById(
-          toChain.supportedCrossChainTokens[0],
-          toChain,
-        ),
-        fromChain: network.name,
-        toChain
-      };
-      const bridge = bridgeConfig ? bridgeConfig : defaultBridgeConfig;
+    // init bridgeConfig
+    const bridgeConfig = store.get('bridge');
+    const crossChainNetworks = _.filter(
+      window.NETWORK_CONFIGS,
+      (v) => v.enabled && v.crossChainSupported,
+    );
+    const toChain = crossChainNetworks.find(
+      (v) => v.chainId !== network.chainId,
+    );
 
-      this.updateBridgeConfig(bridge);
-    }
+    const defaultBridgeConfig = {
+      from: TokenListManager.findTokenById(network.supportedCrossChainTokens[0]),
+      to: TokenListManager.findTokenById(
+        toChain.supportedCrossChainTokens[0],
+        toChain,
+      ),
+      fromChain: network.name,
+      toChain
+    };
+    const bridge = bridgeConfig ? bridgeConfig : defaultBridgeConfig;
+
+    this.updateBridgeConfig(bridge);
   },
 
-  // TODO need to refactor this
   updateSwapConfig: function (swap) {
     this.swap = _.extend(this.getSwapConfig(), swap);
     store.set('swap', this.swap);
