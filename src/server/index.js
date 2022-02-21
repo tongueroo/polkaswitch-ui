a2dff53c2ad92ab71031f2fe38b814359ac8e4ff
reaper <kiru@swing.xyz>
2022-02-20 20:18:21 -0800

    Test

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,6 @@ var Sentry = require('@sentry/node');
 var Tracing = require('@sentry/tracing');
 
 var passport = require('./middleware/auth');
-// var redis = require('./middleware/redis');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const app = express();
