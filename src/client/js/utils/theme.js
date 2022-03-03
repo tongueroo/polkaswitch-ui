8a3fb53db2d0b41694877c2ca4a28c4f969e4b2b
ponyjackal <pony1125@outlook.com>
2022-03-03 10:17:53 -0800

    revert changes in themejs

--- src/client/js/utils/theme.js
+++ src/client/js/utils/theme.js
@@ -4,16 +4,15 @@ function setTheme(themeName) {
 }
 
 function keepTheme() {
-  // if (localStorage.getItem('theme')) {
-  //   if (localStorage.getItem('theme') === 'theme-dark') {
-  //     setTheme('theme-dark');
-  //   } else if (localStorage.getItem('theme') === 'theme-light') {
-  //     setTheme('theme-light');
-  //   }
-  // } else {
-  //   setTheme('theme-light');
-  // }
-  setTheme('theme-dark');
+  if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-dark');
+    } else if (localStorage.getItem('theme') === 'theme-light') {
+      setTheme('theme-light');
+    }
+  } else {
+    setTheme('theme-light');
+  }
 }
 
 module.exports = {
