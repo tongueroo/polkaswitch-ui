9980ec1eb3da3f70d30f2b9f0b6e56109ccc4f6f
chillvybz <85249140+chillvybz@users.noreply.github.com>
2022-03-07 17:49:15 -0500

    Revert "Updated new polygon aggregator"

--- src/client/js/utils/swapFn.js
+++ src/client/js/utils/swapFn.js
@@ -287,7 +287,7 @@ window.SwapFn = {
       );
 
       let expectReturnResult = null;
-      if (chainId === '56' || chainId === '137') {
+      if (chainId === '56') {
         expectReturnResult = await contract.getExpectedReturn(
           fromToken.address,
           toToken.address,
@@ -346,7 +346,6 @@ window.SwapFn = {
             amountBN,
             expectedAmount,
             minReturn,
-            recipient,
             distribution,
           );
         case '1285':
