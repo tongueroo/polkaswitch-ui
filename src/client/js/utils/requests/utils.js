54017b6b20e34f73f9b29ad8ba9c492b0fc81adb
Yuri Ramos <yuri.pramos@gmail.com>
2022-03-17 19:12:17 +0200

    Add `requests/utils`

---
+++ src/client/js/utils/requests/utils.js
@@ -0,0 +1,6 @@
+// 'https://api.dev.swing.xyz'
+
+// Temporary baseUrl until merge /transfer and /claim
+const baseUrl = 'https://crosschain-swap-api-web-frontend.dev.swing.xyz';
+
+export { baseUrl };
