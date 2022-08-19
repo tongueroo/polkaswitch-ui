4674e5997fce8fce0b02d04d0e6263330d269ef3
Yuri Ramos <yuri.pramos@gmail.com>
2022-08-19 10:26:40 +0200

    Add Swing Swap Widget

--- src/client/js/components/pages/SwapHome.jsx
+++ src/client/js/components/pages/SwapHome.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import TokenPairChartOverview from '../partials/swap/TokenPairChartOverview';
 import Navbar from '../partials/navbar/Navbar';
 import SwapWidget from '../partials/swap/SwapWidget';
-import AlphaOnboardingCard from '../partials/swap/AlphaOnboardingCard';
-import SwapNetworkToggle from '../partials/swap/SwapNetworkToggle';
 import ConnectWalletModal from '../partials/ConnectWalletModal';
 import TxHistoryModal from '../partials/TxHistoryModal';
 import NotificationSystem from '../partials/NotificationSystem';
 import MobileMenu from '../partials/navbar/MobileMenu';
 import TxnHistory from '../partials/swap/TxnHistory';
-import Search from '../partials/swap/Search';
+import '@swing.xyz/ui/theme.css';
+import { Swap } from '@swing.xyz/ui';
 
 export default class Home extends Component {
   render() {
@@ -20,21 +18,9 @@ export default class Home extends Component {
         <NotificationSystem />
         <ConnectWalletModal />
         <TxHistoryModal />
-        <div className="columns">
-          <div className="column trading-view-column" style={{ minWidth: 0 }}>
-            {/*<Search />*/}
-            <div className="box trading-view-wrapper">
-              <TokenPairChartOverview />
-            </div>
-            <div>
-              <TxnHistory />
-            </div>
-          </div>
-          <div className="column swap-column" style={{ margin: '0 auto' }}>
-            <SwapNetworkToggle />
-            <SwapWidget />
-            <AlphaOnboardingCard />
-          </div>
+        <div>
+          <Swap />
+          <TxnHistory />
         </div>
       </div>
     );
